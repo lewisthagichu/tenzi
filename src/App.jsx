309cf0ec4942204305e5c3a69d0a3401dac4d666
lewisthagichu <lewisthagichu254@gmail.com>
2023-11-20 16:34:37 +0300

    build: Create and style dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
+import Die from './components/Die'
 
 function App() {
-  const [dice, setDice] = useState({})
+  const [dice, setDice] = useState(createNewDice());
 
+  function createNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(
+        {
+          id: nanoid(),
+          value: Math.ceil(Math.random() * 6)
+        }
+      )
+    } 
+    return newDice
+  }
+  const diceElements = dice.map(die => (
+      <Die key={die.id} value={die.value} />
+    ))
+ 
   return (
-    <main>Hello</main>
+    <main>
+       <div className="dice-container">
+          {diceElements}
+        </div>
+    </main>
   )
   
 }
