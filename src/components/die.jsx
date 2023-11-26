e0eed55afe6ba2bfb27d8e5945993726d3d309a8
lewisthagichu <lewisthagichu254@gmail.com>
2023-11-26 16:41:00 +0300

    build: Add dots on the dice

--- src/components/die.jsx
+++ src/components/die.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Die(props) {
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white" 
+        backgroundColor: props.isHeld ? "#F5E000" : "white" 
     }
     return (
         <div className="die-face" style={styles} onClick={props.holdDice}>
-            <h2>{props.value}</h2>
+            {Array.from({ length: props.value }).map((_, index) => (
+        <div key={index} className="dot"></div>
+      ))}
         </div>
     )
 }
